4389b9beb9b9039fcef9790fcecf13e545e540bd
DedPirded <48673097+DedPirded@users.noreply.github.com>
2020-04-03 16:34:45 +0300

    just update
    add some fichessss

--- ConsoleAppPM/ConsoleAppPM/Program.cs
+++ ConsoleAppPM/ConsoleAppPM/Program.cs
@@ -9,22 +9,26 @@ namespace ConsoleAppPM
             //Console.WriteLine("Hell to World!");
             Console.WriteLine("Выберете операцию: 1 - INN с 10 символами; 2 - INN с 14 символами");
             int numChoice = Convert.ToInt32(Console.ReadLine());
-            switch(numChoice)
+            switch (numChoice)
             {
-                case 1:               
-                    Console.WriteLine("Напишите ваш INN");
+                case 1:
+                    Console.WriteLine("Напишите ваш INN(писать по одному символу)");
                     int[] inn = new int[10];
                     DeclarArray(inn);
                     bool checkAnswer;
+                    int year;
+                    int day;
+                    int month;
                     CheckINN(inn, out checkAnswer);
-                    WriteAnswer(checkAnswer);
+                    CheckBirthday(inn, out year, out month, out day);
+                    WriteAnswer(checkAnswer, CheckSex(inn), year, month, day);
                     break;
                 case 2:
                     Console.WriteLine("Данная функция пока не реализована");
                     break;
-                               
+
             }
-            
+
         }
         static int[] DeclarArray(int[] x)          // Записываем INN в виде массива, где каждый элемент массива является одним символом   
         {
@@ -49,12 +53,68 @@ namespace ConsoleAppPM
                 checkAnswer = false;
 
         }
-        static void WriteAnswer(bool x)
+        static void CheckBirthday(int[] inn, out int year, out int month, out int day)
+        {
+            year = 1900;
+            day = 01;
+            month = 01;
+            int num = inn[0] * 10000 + inn[1] * 1000 + inn[2] * 100 + inn[3] * 10 + inn[4];
+            while (num > 365)
+            {
+                if (year % 4 == 0)
+                {
+                    num -= 366;
+                    year++;
+                }
+                else
+                {
+                    num -= 365;
+                    year++;
+                }
+            }
+            while (num > 30)
+            {
+                if (month == 2)
+                {
+                    num -= 28;
+                    month++;
+                }
+                else
+                {
+                    if (month % 2 == 0)
+                    {
+                        num -= 30;
+                        month++;
+                    }
+                    else
+                    {
+                        num -= 31;
+                        month++;
+                    }
+                }
+            }
+            day += num;
+
+            /*Console.WriteLine(num);
+            Console.WriteLine(years);
+            Console.WriteLine(month);
+            Console.WriteLine(day);*/
+        }
+        static string CheckSex(int[] inn)
+        {
+            if (inn[8] % 2 == 0)
+                return "Female";
+            else
+                return "Male";
+        }
+        static void WriteAnswer(bool x, string answerSex, int year, int month, int day)
         {
             if (x == true)
                 Console.WriteLine("INN верный");
             else
                 Console.WriteLine("INN не верный");
+            Console.WriteLine("Пол - " + answerSex);
+            Console.WriteLine($"День вашего рождения: {year}.{month}.{day}");
         }
     }
 }
